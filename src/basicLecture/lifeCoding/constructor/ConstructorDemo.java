7ae3d35e3212bed4a3b1673ea714819fabf8411a
JungYoonweon <wjddbsdnjs91@naver.con>
2019-11-03 23:44:30 +0900

    abc

--- src/basicLecture/lifeCoding/constructor/ConstructorDemo.java
+++ src/basicLecture/lifeCoding/constructor/ConstructorDemo.java
@@ -3,5 +3,9 @@ package basicLecture.lifeCoding.constructor;
 public class ConstructorDemo {
 	public static void main(String[] args) {
 		ConstructorDemo c = new ConstructorDemo();
+		
+		int a = 3;
+		float b = 1.0F;
+		double cv = a + b;
 	}
 }
